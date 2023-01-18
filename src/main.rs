76972739fc3801d66f78307b0b3b3d0e251ed200
flooijdt <flooijdt@posteo.de>
2023-01-18 18:42:58 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use yew::prelude::*;
 
+struct Counter {
+    counter: u32,
+}
+
 #[function_component(App)]
 fn app() -> Html {
+    let state = use_state(|| Counter { counter: 0 });
+
+    let onclick = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Counter {
+                counter: state.counter + 1,
+            })
+        })
+    };
+
     html!(
-        <h1>{"Hi"}</h1>
+        <>
+        <label>{ state.counter }</label><br/>
+        <button {onclick}>{"+1"}</button>
+        </>
     )
 }
 
